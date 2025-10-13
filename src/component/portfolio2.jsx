2c268c13632fd96ece73642f571fcb93a6177d3b
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-14 01:32:42 +0500

    update-image

--- src/component/portfolio2.jsx
+++ src/component/portfolio2.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Arrowimag from 'src/assets/down-arrow.png'
 
 function Portfolio2() {
     return (
@@ -6,7 +7,7 @@ function Portfolio2() {
             <h1 className='text-[90px] font-extrabold'>
                 Featured <span className='text-[#8e827c] pb-10'>work</span>
             </h1>
-            <img src="src/assets/down-arrow.png" className='w-15 h-15'></img>
+            <img src={Arrowimag} className='w-15 h-15'></img>
             <p className="text-[20px] md:text-[24px] leading-relaxed text-gray-700 w-[400px] pt-10 ">
                 We create innovative and purposeful designs that not only capture attention but also drive meaningful results.
             </p>
