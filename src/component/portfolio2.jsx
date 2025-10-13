2a24ee1618bad30a353059b42efe215778c74fce
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-14 01:27:35 +0500

    change imagee

--- src/component/portfolio2.jsx
+++ src/component/portfolio2.jsx
@@ -4,7 +4,7 @@ function Portfolio2() {
     return (
         <div className='flex  flex-col justify-center items-center'>
             <h1 className='text-[90px] font-extrabold'>
-                Featured <span className='text-[#8e827c] pb-10'>work</span>
+                Featured <span className='text-[#8e827c] pb-10'>work.</span>
             </h1>
             <img src="src/assets/down-arrow.png" className='w-15 h-15'></img>
             <p className="text-[20px] md:text-[24px] leading-relaxed text-gray-700 w-[400px] pt-10 ">
