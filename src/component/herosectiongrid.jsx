a4f6c18ca5ce7a0ec9cf426b233b8b3f4eed72c5
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-18 23:43:09 +0500

    update

--- src/component/herosectiongrid.jsx
+++ src/component/herosectiongrid.jsx
@@ -2,24 +2,37 @@ import React from 'react'
 
 const Herosectiongrid = () => {
   return (
-    <div>
-  <div class="grid grid-cols-[1fr_1.5fr] grid-rows-[1fr_2fr] gap-3 w-full ">
-    
-    <div class="row-span-2 bg-fuchsia-600 rounded-xl flex items-center justify-center text-white text-2xl font-bold">
-      01
-    </div>
+    <div
+      class="grid grid-cols-[1fr_1.2fr_1fr] grid-rows-2 gap-4 w-[90%] max-w-[1200px] h-[80vh]"
+    >
+      {/* <!-- Left Full Height --> */}
+      <div
+        class="row-span-2 bg-fuchsia-600 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+      >
+        01
+      </div>
+{/* 
+      <!-- Top Middle --> */}
+      <div
+        class="bg-purple-700 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+      >
+        02
+      </div>
+{/* 
+      <!-- Bottom Middle --> */}
+      <div
+        class="bg-pink-600 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+      >
+        03
+      </div>
 
-
-    <div class="bg-purple-800 rounded-xl flex items-center justify-center text-fuchsia-300 text-2xl font-bold">
-      02
+      {/* <!-- Right Full Height --> */}
+      <div
+        class="row-span-2 bg-fuchsia-400 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+      >
+        04
+      </div>
     </div>
-
-  
-    <div class="bg-fuchsia-500 rounded-xl flex items-center justify-center text-white text-2xl font-bold">
-      03
-    </div>
-  </div>
-</div>
 
   )
 }
