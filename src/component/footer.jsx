2eac6a5aca5d65b1b0b7c6436ec18ab31708ec91
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-19 00:23:55 +0500

    update

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-black flex '>
+    <div className='bg-black flex justify-between pt-10 pb-10 pr-10 pl-10'>
         <h1 className='text-white'>
             c© 2025 Abdul Rahim. All Rights Reserved.
         </h1>
