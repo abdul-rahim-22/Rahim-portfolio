a8c1e90eb7dc362482acec9d9f80a736c2832c87
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-18 23:14:53 +0500

    update

--- src/component/Herosection.jsx
+++ src/component/Herosection.jsx
@@ -71,6 +71,25 @@ function Herosection() {
           {/* Note */}
           <p className="text-[#888888] text-sm mt-3">Yes, that’s my resume</p>
         </div>
+        <div class="min-h-screen bg-gray-900 flex items-center justify-center p-6">
+  <div class="grid grid-cols-[1fr_1.5fr] grid-rows-[1fr_2fr] gap-3 w-full max-w-6xl">
+    
+    <div class="row-span-2 bg-fuchsia-600 rounded-xl flex items-center justify-center text-white text-2xl font-bold">
+      01
+    </div>
+
+
+    <div class="bg-purple-800 rounded-xl flex items-center justify-center text-fuchsia-300 text-2xl font-bold">
+      02
+    </div>
+
+    
+    <div class="bg-fuchsia-500 rounded-xl flex items-center justify-center text-white text-2xl font-bold">
+      03
+    </div>
+  </div>
+</div>
+
       </div>
     </section>
   );
