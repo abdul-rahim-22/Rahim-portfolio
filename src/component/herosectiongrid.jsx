63e42080141cf5d87fcecbeed90a9973e5d7b644
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-18 23:48:48 +0500

    update

--- src/component/herosectiongrid.jsx
+++ src/component/herosectiongrid.jsx
@@ -2,38 +2,39 @@ import React from 'react'
 
 const Herosectiongrid = () => {
   return (
-    <div
-      class="grid grid-cols-[1fr_1.2fr_1fr] grid-rows-2 gap-4 w-[90%] max-w-[1200px] h-[80vh]"
+  <div>
+ <div
+      class="grid grid-cols-[1fr_1.2fr_1fr] grid-rows-2 gap-4 w-[90%] max-w-[1200px] h-[80vh]
+             md:grid-cols-1 md:grid-rows-[repeat(4,200px)] md:h-auto"
     >
-      {/* <!-- Left Full Height --> */}
       <div
-        class="row-span-2 bg-fuchsia-600 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+        class="row-span-2 bg-[#e600ff] rounded-xl flex items-center justify-center text-3xl font-bold transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)]"
       >
         01
       </div>
-{/* 
-      <!-- Top Middle --> */}
+
+     
       <div
-        class="bg-purple-700 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+        class="bg-[#b300ff] rounded-xl flex items-center justify-center text-3xl font-bold transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)]"
       >
         02
       </div>
-{/* 
-      <!-- Bottom Middle --> */}
+
+
       <div
-        class="bg-pink-600 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+        class="bg-[#ff00aa] rounded-xl flex items-center justify-center text-3xl font-bold transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)]"
       >
         03
       </div>
 
-      {/* <!-- Right Full Height --> */}
+   
       <div
-        class="row-span-2 bg-fuchsia-400 rounded-xl flex items-center justify-center text-white text-3xl font-bold hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)] transition-transform duration-300"
+        class="row-span-2 bg-[#ff00ff] rounded-xl flex items-center justify-center text-3xl font-bold transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(255,0,255,0.4)]"
       >
         04
       </div>
     </div>
-
+  </div>
   )
 }
 
