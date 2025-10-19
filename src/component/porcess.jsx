309e322973683c9ecf2e29804dfdcd8c60ac446b
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-19 20:06:47 +0500

    update

--- src/component/porcess.jsx
+++ src/component/porcess.jsx
@@ -8,7 +8,7 @@ function Porcess() {
                 ⚙️ Process <span className='text-[#8e827c] '>/ Workflow</span>
             </h1>
             <img src='https://cdn-icons-png.flaticon.com/512/8841/8841304.png' className='w-15 h-15'></img>
-            <p className="text-[20px] md:text-[24px] leading-relaxed text-gray-700 w-[600px]">
+            <p className="text-[20px] md:text-[24px] leading-relaxed text-gray-700 w-[900px]">
                 My work process is simple but structured — built to deliver quality, clarity, and results. From understanding your goals to launching a fast, user-friendly website, every step is handled with clear communication and attention to detail.
             </p>
             <h2 className="text-3xl md:text-4xl font-semibold text-gray-800 mb-6 pt-10"> 
