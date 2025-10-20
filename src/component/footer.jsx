dc50f96ba5fdf743f18527f90f778c45b90b531f
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-20 21:34:53 +0500

    update

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -4,10 +4,10 @@ import LocalTime from './localtime'
 function Footer() {
   return (
    <div className="bg-black flex flex-col md:flex-row items-center justify-between gap-3 md:gap-0 text-center md:text-left py-5 px-6 md:px-10">
-  <h1 className="text-white text-sm md:text-base">
+  <h1 className="text-white text-[18px] md:text-base">
     © 2025 Abdul Rahim. All Rights Reserved.
   </h1>
-  <h1 className="text-white text-sm md:text-base">
+  <h1 className="text-white text-[18px] md:text-base">
    <span className='inline-flex'><LocalTime/></span> Follow us on LinkedIn | Instagram |
   </h1>
 </div>
