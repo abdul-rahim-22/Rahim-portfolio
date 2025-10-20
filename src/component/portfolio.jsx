874dea4de5dd5dc36b8f7af0f5dfaf81640facfe
💸• • •A B D U L R A H I M• • •💵 <abdulrahim@MacBook-Pro.local>
2025-10-20 21:21:08 +0500

    update

--- src/component/portfolio.jsx
+++ src/component/portfolio.jsx
@@ -7,7 +7,7 @@ function Portfolio() {
 
 
   return (
-    <div className=" w-full  flex flex-col items-center justify-center text-center  overflow-hidden bg-[#FEFDF8] pb-20 pt-10  " >
+    <div className=" w-full  flex flex-col items-center justify-center text-center  overflow-hidden bg-[#FEFDF8] py-10 sm:py-14 md:py-16 lg:py-20  " >
     <div className="w-[1300px]  ">
     <h2 className="text-3xl md:text-4xl font-semibold text-gray-800 mb-6">
           {"{ Portfolio }"}
