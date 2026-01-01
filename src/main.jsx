c23b807bfbf83bac727f97342c93efa5feef4c46
Abdul-Rahim <raheemjutt75@gmail.com>
2026-01-01 16:31:05 +0500

    Import and use SkillIncomeMatcher component

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import SplashCursor from './component/cursor.jsx'
 import Footer from './component/footer.jsx'
 import Processs from './component/porcess.jsx'
 import AIArticleWriter from './component/AIArticleWriter.jsx';
+import SkillIncomeMatcher from ".component/SkillIncomeMatcher.jsx";
 
 
 
@@ -32,6 +33,7 @@ createRoot(document.getElementById('root')).render(
   <Portfolio/>
   <Processs/>
 {/* <AIArticleWriter/>  */}
+    <SkillIncomeMatcher />
   <Footer/>
   </>
  
